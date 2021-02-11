55a89df9d5a9dddba8a3647a71f88fb3c4043d76
Jyunaut <edmartinh@gmail.com>
2021-02-10 21:39:34 -0800

    cleaned up motor code

--- test/test.ino
+++ test/test.ino
@@ -1,40 +1,72 @@
-#define pin_PWM       10
-#define pin_ENCODER   3
-#define ENCODEROUTPUT 980
-#define HALLSEN_A     3
+#define PIN_PWM     10
+#define PIN_INA     12
+#define PIN_INB     13
+#define PIN_ENCODER 3
+#define ENCODERCPR  980
 
 volatile long encoderVal = 0;
 
-int interval = 1000;
+int interval    = 1000;
 long prevMillis = 0;
-long currentMillis = 0;
+long curMillis  = 0;
 
-int rpm = 0;
+int rpm         = 0;
+int motorPWM    = 0;
 bool measureRPM = false;
-int motorPWM = 0;
 
-// hello
+int test = 127;
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(57600);
+    
+    EncoderInit();
+    PinInit();
 
-    pinMode(10, OUTPUT);
-    pinMode(3, INPUT_PULLUP);
-    pinMode(12, OUTPUT);
-    pinMode(13, OUTPUT);
+    prevMillis = millis();
 }
 
 void loop()
 {
+    // Update RPM value each second
+    curMillis = millis();
+    if (curMillis - prevMillis > interval) {
+        prevMillis = curMillis;
+    }
+
     digitalWrite(12, HIGH);
     digitalWrite(13, LOW);
-    analogWrite(pin_PWM, 50);
-    
-    delay(1500);
-    
-    while(1) {
-        digitalWrite(12, LOW);
-        digitalWrite(13, LOW);
+    analogWrite(PIN_PWM, test);
+
+    rpm = (float)(encoderVal * 60 / ENCODERCPR);
+    if (rpm > 0) {
+        Serial.print(encoderVal);
+        Serial.print(" pulse / ");
+        Serial.print(ENCODERCPR);
+        Serial.print(" pulse per rotation * 60 seconds = ");
+        Serial.print(rpm);
+        Serial.println(" RPM");
     }
+    encoderVal = 0;
+}
+
+void PinInit()
+{
+    pinMode(PIN_PWM, OUTPUT);
+    pinMode(PIN_INA, OUTPUT);
+    pinMode(PIN_INB, OUTPUT);
+    pinMode(PIN_ENCODER, INPUT_PULLUP);
+}
+
+// Attach interrupt at hall sensor A on each rising signal
+void EncoderInit()
+{
+    attachInterrupt(digitalPinToInterrupt(PIN_ENCODER), UpdateEncoder, CHANGE);
+    encoderVal = 0;
+}
+
+// Add encoderVal by 1 each time it detects a rising signal from hall sensor A
+void UpdateEncoder()
+{
+    encoderVal++;
 }
